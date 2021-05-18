6aedcf8f3c0321942931284320337eae8da0fcdc
aninarafath6 <arafathanin6@gmail.com>
2021-05-18 17:02:03 +0530

    requsts

---
+++ utils/requests.js
@@ -0,0 +1,50 @@
+export default {
+  fetchFastFood: {
+    title: "Fast Food",
+    url: "/api/dish/Fast Food",
+  },
+  fetchGrill: {
+    title: "Grill",
+    url: "/api/dish/grill",
+  },
+  fetchPastas: {
+    title: "Pastas",
+    url: "/api/dish/top_of_the_week",
+  },
+  fetchPizza: {
+    title: "Pizza",
+    url: "/api/dish/pizza",
+  },
+  fetchSalads: {
+    title: "Pizza",
+    url: "/api/dish/pizza",
+  },
+  fetchSnacks: {
+    title: "Snacks",
+    url: "/api/dish/pizza",
+  },
+  fetchSweets: {
+    title: "Sweets",
+    url: "/api/dish/pizza",
+  },
+  fetchBakery: {
+    title: "Bakery",
+    url: "/api/dish/pizza",
+  },
+  fetch: {
+    title: "Juices",
+    url: "/api/dish/pizza",
+  },
+  fetch: {
+    title: "Kebabs",
+    url: "/api/dish/pizza",
+  },
+  fetchIceCream: {
+    title: "Ice Cream",
+    url: "/api/dish/pizza",
+  },
+  fetchBiryani: {
+    title: "Biryani",
+    url: "/api/dish/pizza",
+  },
+};
