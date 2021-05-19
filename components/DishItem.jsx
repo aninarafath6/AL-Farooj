b301c2c119e3d04172c2907cc7c5d7d844a914eb
aninarafath6 <arafathanin6@gmail.com>
2021-05-19 16:49:10 +0530

    cheanged grid layout

--- components/DishItem.jsx
+++ components/DishItem.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function DishItem({ dish }) {
   return (
-    <div className="  group p-2 transition duration-200 transform sm:hover:scale-105 hover:z-50 cursor-pointer">
+    <div className=" h-auto group p-2 transition duration-200 transform sm:hover:scale-105 hover:z-50 cursor-pointer">
       <Image
         className="rounded"
         objectFit="cover"
@@ -17,13 +17,13 @@ function DishItem({ dish }) {
         <p className="italic mt-2">
           ₹ {dish.price} <span className="p-1 mx-2 rounded">{dish.type}</span>
         </p>
-        <h2 className="text-2xl mt-1 text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+        <h2 className="truncate mt-1 text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {dish.name}
         </h2>
         <button
           onClick={() => alert("item added to cart")}
           type="button"
-          className="focus:outline-none hover:bg-primary-dark bg-primary-light py-1 px-2 rounded my-2 outline-none border-none  cursor-pointer text-sm xl:text-lg opacity-0 group-hover:opacity-100"
+          className="focus:outline-none hover:bg-primary-dark bg-primary-light py-1 px-2 rounded my-2 outline-none border-none  cursor-pointer text-sm  opacity-0 group-hover:opacity-100"
         >
           ADD TO CART
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,26 @@ module.exports = {
         },
       },
       screens: {
-        "3xl": "2000px",
+        xs: "375px",
+        // => @media (min-width: 375px) { ... }
+
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': "1536px",
+        // => @media (min-width: 1536px) { ... }
+
+        '3xl': "2000px",
+        // => @media (min-width:2000px) { ... }
       },
     },
   },
