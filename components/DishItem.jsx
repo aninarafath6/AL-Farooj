3bbb491201f8d230bbac919eaff30193d27ea8f7
aninarafath6 <arafathanin6@gmail.com>
2021-05-19 16:14:46 +0530

    image rounded

--- components/DishItem.jsx
+++ components/DishItem.jsx
@@ -5,6 +5,7 @@ function DishItem({ dish }) {
   return (
     <div className="  group p-2 transition duration-200 transform sm:hover:scale-105 hover:z-50 cursor-pointer">
       <Image
+        className="rounded"
         objectFit="cover"
         layout="responsive"
         width={1920}
