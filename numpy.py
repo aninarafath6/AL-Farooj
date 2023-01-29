4f60933aaee0d4867e0e51c74a4129929b3bd96d
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2023-01-30 01:04:31 +0530

    Created using Colaboratory

---
+++ numpy.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPFKlJENxmCi2DBCRboz2SU",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/aninarafath6/AL-Farooj/blob/main/numpy.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "CZa9Bcd4BXbz"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
